17ca75eaf3c2736c69100a4e5ef681560603526e
hikenrides <cephas@hikenrides.com>
2023-11-19 14:44:27 +0200

    ....

--- api/allowCors.js
+++ api/allowCors.js
@@ -1,6 +1,5 @@
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'https://hikenrides.com')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'https://hikenrides.com')
