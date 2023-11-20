3f649469e07ff5e9497a645d5a3271d3f85f8940
hikenrides <cephas@hikenrides.com>
2023-11-20 22:42:09 +0200

    cors

--- api/index.js
+++ api/index.js
@@ -24,10 +24,7 @@ const bucket = 'hikenrides-booking-app';
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use(cors({
-  credentials: true,
-  origin: 'https://hikenrides.com',
-}));
+app.use(cors());
 
 
 
