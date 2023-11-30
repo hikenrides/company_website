3a807d4a0c6f2b7238573e10a806469bf906135e
hikenrides <cephas@hikenrides.com>
2023-11-30 21:37:03 +0200

    search engine

--- client/src/FindCarForm.jsx
+++ client/src/FindCarForm.jsx
@@ -2,33 +2,54 @@ import React, { useState } from "react";
 import "./find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
 
+const provinces = [
+  "Eastern Cape",
+  "Free State",
+  "Gauteng",
+  "KwaZulu-Natal",
+  "Limpopo",
+  "Mpumalanga",
+  "North West",
+  "Northern Cape",
+  "Western Cape",
+];
+
 const FindCarForm = ({ onSearch }) => {
-  const [fromLocation, setFromLocation] = useState("");
-  const [toLocation, setToLocation] = useState("");
+  const [selectedProvince, setSelectedProvince] = useState("");
+  const [destination, setDestination] = useState("");
 
   const handleSearch = () => {
-    onSearch({ fromLocation, toLocation });
+    // Call the onSearch prop with the selected province and destination
+    onSearch(selectedProvince, destination);
   };
 
   return (
     <Form className="form">
       <div className="d-flex align-items-center justify-content-between flex">
         <FormGroup className="form__group">
-          <input
-            type="text"
-            placeholder="From location"
-            value={fromLocation}
-            onChange={(e) => setFromLocation(e.target.value)}
+          {/* Use a dropdown for "From location" */}
+          <select
+            value={selectedProvince}
+            onChange={(e) => setSelectedProvince(e.target.value)}
             required
-          />
+          >
+            <option value="" disabled>
+              Select From location
+            </option>
+            {provinces.map((province) => (
+              <option key={province} value={province}>
+                {province}
+              </option>
+            ))}
+          </select>
         </FormGroup>
 
         <FormGroup className="form__group flex">
           <input
             type="text"
-            placeholder="To location"
-            value={toLocation}
-            onChange={(e) => setToLocation(e.target.value)}
+            placeholder="Destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
             required
           />
           <button type="button" onClick={handleSearch}>
--- client/src/pages/TripOfferPage.jsx
+++ client/src/pages/TripOfferPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col } from "reactstrap"; 
 import { Link } from "react-router-dom";
 import FindCarForm from "../FindCarForm";
 
@@ -18,39 +18,37 @@ const provinces = [
 
 export default function TripOfferPage() {
   const [places, setPlaces] = useState([]);
-  const [selectedProvince, setSelectedProvince] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchError, setSearchError] = useState(false);
+  const [selectedProvince, setSelectedProvince] = useState('');
 
   useEffect(() => {
-    axios.get("/places").then((response) => {
+    axios.get('/places').then(response => {
       setPlaces(response.data);
     });
   }, []);
 
   const handleProvinceSelect = (province) => {
-    setSelectedProvince((prevState) =>
-      prevState === province ? "" : province
-    );
-  };
+    setSelectedProvince(prevState => prevState === province ? '' : province);
 
-
-  const handleSearch = (fromLocation, toLocation) => {
-    const matchingPlaces = places.filter(
-      (place) =>
-        place.from.toLowerCase().includes(String(fromLocation).toLowerCase()) &&
-        place.destination.toLowerCase().includes(String(toLocation).toLowerCase()) &&
-        (selectedProvince ? place.province === selectedProvince : true)
-    );
   
-    if (matchingPlaces.length > 0) {
-      setSearchResults(matchingPlaces);
-      setSearchError(false);
-    } else {
-      setSearchResults([]);
-      setSearchError(true);
-    }
   };
+
+  const handleSearch = (fromLocation, destination) => {
+    // Filter places based on selected province and destination
+    const matchingPlaces = places.filter((place) => {
+      const normalizedDestination = place.destination.toLowerCase();
+      const normalizedInput = destination.toLowerCase();
+  
+      return (
+        (selectedProvince ? place.province === selectedProvince : true) &&
+        normalizedDestination.includes(normalizedInput)
+      );
+    });
+  
+    // Update your UI with the matching places
+    // You can use this matchingPlaces array in your rendering logic
+    console.log(matchingPlaces);
+  };
+  
 
   return (
     <div className="mt-20">
@@ -65,7 +63,8 @@ export default function TripOfferPage() {
               </Col>
 
               <Col lg="8" md="8" sm="12">
-                <FindCarForm onSearch={handleSearch} />
+              <FindCarForm onSearch={handleSearch} />
+
               </Col>
             </Row>
           </Container>
@@ -76,59 +75,29 @@ export default function TripOfferPage() {
           <h2
             className="cursor-pointer bg-gray-300 p-4 rounded-2xl flex justify-between items-center"
             onClick={() => handleProvinceSelect(province)}
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: '16px' }}
           >
-            {province} {selectedProvince === province ? "▲" : "▼"}
+            {province} {selectedProvince === province ? '▲' : '▼'}
           </h2>
-
-          {selectedProvince === province &&
-            (searchResults.length > 0 ? (
-              searchResults.map((place) => (
-                <Link
-                  key={place._id}
-                  to={"/place/" + place._id}
-                  className="block cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
-                  style={{ marginBottom: "16px" }}
-                >
-                  <h2 className="font-bold">
-                    <span style={{ color: "orange" }}>pick-up area:</span>{" "}
-                    {place.destination}
-                  </h2>
-                  <h3 className="text-sm text-gray-500">
-                    <span style={{ color: "orange" }}>Destination:</span>{" "}
-                    {place.from}
-                  </h3>
-                  <div className="mt-1">
-                    <span className="font-bold">R{place.price}</span> per person
-                  </div>
-                </Link>
-              ))
-            ) : searchError ? (
-              <p>No trips found with the entered location.</p>
-            ) : (
-              places
-                .filter((place) => place.province === province)
-                .map((place) => (
-                  <Link
-                    key={place._id}
-                    to={"/place/" + place._id}
-                    className="block cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
-                    style={{ marginBottom: "16px" }}
-                  >
-                    <h2 className="font-bold">
-                      <span style={{ color: "orange" }}>pick-up area:</span>{" "}
-                      {place.destination}
-                    </h2>
-                    <h3 className="text-sm text-gray-500">
-                      <span style={{ color: "orange" }}>Destination:</span>{" "}
-                      {place.from}
-                    </h3>
-                    <div className="mt-1">
-                      <span className="font-bold">R{place.price}</span> per person
-                    </div>
-                  </Link>
-                ))
-            ))}
+          
+          {selectedProvince === province && places.filter(place => place.province === province).map(place => (
+            <Link
+              key={place._id}
+              to={'/place/' + place._id}
+              className="block cursor-pointer gap-4 bg-gray-300 p-4 rounded-2xl"
+              style={{ marginBottom: '16px' }}
+            >
+              <h2 className="font-bold">
+                <span style={{ color: 'orange' }}>pick-up area:</span> {place.destination}
+              </h2>
+              <h3 className="text-sm text-gray-500">
+                <span style={{ color: 'orange' }}>Destination:</span> {place.from}
+              </h3>
+              <div className="mt-1">
+                <span className="font-bold">R{place.price}</span> per person
+              </div>
+            </Link>
+          ))}
         </div>
       ))}
     </div>
