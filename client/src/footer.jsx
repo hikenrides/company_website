05ff9170a6f31c1ccaab22726711b0d26e65d27d
hikenrides <cephas@hikenrides.com>
2023-12-12 00:55:31 +0200

    importing error

--- client/src/footer.jsx
+++ client/src/footer.jsx
@@ -1,80 +1,30 @@
 import React from "react";
-import './footer.css';
-import fb from './assets/fbimg.png';
-import twitter from './assets/twitterimg.png';
-import linkedin from './assets/linkedinimg.png';
-import insta from './assets/instaimg.png';
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./footer";
 
-const Footer= () =>{
-    return (
-        <div className="footer mb-0">
-            <div className="sb__footer section__padding">
-                <div className="sb__footer-links">
-                    
-                    <div className="sb__footer-links_div">
-                        <h4>Resources</h4>
-                        <a href="/resource">
-                            <p>Resource Center</p>
-                        </a>
-                        <a href="/resource">
-                            <p>Testimonials</p>
-                        </a>
-                        <a href="/resource">
-                            <p>STV</p>
-                        </a>
-                    </div>
-                    <div className="sb__footer-links_div">
-                        <h4>Contact Us
-                        </h4>
-                        <p>Whatsapp: +27 645388351</p>
-                        <p>Email: info@hikenrides.com</p>
-                    </div>
-                    <div className="sb__footer-links_div">
-                    <h4>Company</h4>
-                        <a href="/about">
-                            <p>About Us</p>
-                        </a>
-                        <a href="/press">
-                            <p>Trips</p>
-                        </a>
-                        <a href="/career">
-                            <p>Requests</p>
-                        </a>
-                        <a href="/contact">
-                            <p>Drivers</p>
-                        </a>
-                        
-                    </div>
-                    <div className="sb__footer-links_div">
-                        <h4>Social media</h4>
-                        <div className="socialmedia">
-                            <a href="https://www.facebook.com/profile.php?id=61553868901955"><p><img src={fb} alt=""/></p></a>
-                            <p><img src={twitter} alt=""/></p>
-                            <p><img src={linkedin} alt=""/></p>
-                            <p><img src={insta} alt=""/></p>
-                        </div>
-                    </div>
-                </div>
+const Layout = () => {
+  const layoutStyle = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh", // Ensure at least viewport height
+  };
 
-                <hr></hr>
+  const contentStyle = {
+    flex: 1, // Allow content to take remaining space
+    background: "linear-gradient(135deg, #FFE53B 0%, #FF2525 74%)",
+    padding: "1rem", // Adjust the padding as needed
+  };
 
-                <div className="sb__footer-below">
-                    <div className="sb__footer-copyright">
-                        <p>
-                            @2023 Hikenrides. All Rights Reserved.
-                        </p>
-                    </div>
-                    <div className="sb__footer-below-links">
-                        <a href="/terms"><div><p>Terms & Conditions</p></div></a>
-                        <a href="/privacy"><div><p>Privacy</p></div></a>
-                        <a href="/security"><div><p>Security</p></div></a>
-                        <a href="/cookies"><div><p>Cookie Declaration</p></div></a>
-                    </div>
-                </div>
+  return (
+    <div style={layoutStyle}>
+      <Header />
+      <div style={contentStyle}>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-            </div>
-        </div>
-    )
-}
-
-export default Footer;
+export default Layout;
