3c4220792fe5fa359f0e78958a36783b395fe8b4
hikenrides <cephas@hikenrides.com>
2023-12-13 23:00:44 +0200

    login error debugging

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Button } from "react-bootstrap";
 import GoogleButton from '/opt/build/repo/client/node_modules/react-google-button';
 import { useUserAuth, UserContext } from "../UserAuthContext.jsx";
-import { onAuthStateChanged } from '/opt/build/repo/client/node_modules/firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 import auth from "../firebase.jsx";
 
 
