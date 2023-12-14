551609979f089bb879ad6cdc72541794893a0c46
hikenrides <cephas@hikenrides.com>
2023-12-14 17:57:09 +0200

    layout styiling

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -60,7 +60,24 @@ export default function Header() {
           </div>
           {!!user && <div className="text-white">{user.name}</div>}
         </Link>
-        <div className="flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" onClick={() => setOpenProfile((prev) => !prev)}>menu</div>
+        <Link
+          to={user ? "/account" : "/login"} 
+        className="flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" onClick={() => setOpenProfile((prev) => !prev)}>
+        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 relative top-1"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          </Link>
         {
                openProfile && <DropDownProfile />
         }
