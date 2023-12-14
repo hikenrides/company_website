3e2cb57001720a8f68a06367253c968778dd72ef
hikenrides <cephas@hikenrides.com>
2023-12-14 18:59:22 +0200

    dropdown menu

--- client/src/DropDownProfile.jsx
+++ client/src/DropDownProfile.jsx
@@ -11,15 +11,6 @@ const DropDownProfile = () => {
     subpage = "profile";
   }
 
-  function linkClasses(type = null) {
-    let classes = "inline-flex gap-1 py-2 px-6 rounded-full";
-    if (type === subpage) {
-      classes += " bg-primary text-white";
-    } else {
-      classes += " bg-gray-200";
-    }
-    return classes;
-  }
 
   return (
     <div className="flex flex-col dropDownProfile">
@@ -30,13 +21,13 @@ const DropDownProfile = () => {
         >
           Profile
         </Link>
-        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+        <Link className="user-container flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" to={"/account/bookings"}>
           My Bookings
         </Link>
-        <Link className={linkClasses("places")} to={"/account/places"}>
+        <Link className="user-container flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" to={"/account/places"}>
           My trip offers
         </Link>
-        <Link className={linkClasses("requests")} to={"/account/Myrequests"}>
+        <Link className="user-container flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" to={"/account/Myrequests"}>
           My trip Requests
         </Link>
       </ul>
