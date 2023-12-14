2f7d19d51ef69f01b1c10737023c56bbd46abc7e
hikenrides <cephas@hikenrides.com>
2023-12-14 23:51:29 +0200

    dropdown menu

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -93,6 +93,7 @@ export default function Header() {
         {/* user-container2: Visible on smartphones */}
         <div  className="user-container2 md:hidden flex items-center gap-2 border border-gray-300 rounded-full py-1 px-1" onClick={() => setOpenProfile((prev) => !prev)} ref={menuRef}>
           <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
