4922e9165ed726e6cbe089de8a7e68271d1f6607
hikenrides <cephas@hikenrides.com>
2023-12-14 23:30:41 +0200

    dropdown menu

--- client/src/DropDownProfile.jsx
+++ client/src/DropDownProfile.jsx
@@ -1,28 +1,36 @@
-import { useContext,useState, useEffect, useRef} from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserAuthContext";
 
 const DropDownProfile = () => {
   const { user } = useContext(UserContext);
   const [open, setOpen] = useState(false);
-  
 
   return (
-    <div className={`flex flex-col dropDownProfile ${open? 'active' : 'inactive'}`}>
+    <div className={`flex flex-col dropDownProfile ${open ? 'active' : 'inactive'}`}>
       <ul className="flex flex-col gap-4">
         <Link
           to={user ? "/account" : "/login"}
-          className="user-container flex items-center gap-2 "
+          className="user-container flex items-center gap-2 text-sm" 
         >
           Profile
         </Link>
-        <Link className="user-container flex items-center gap-2 " to={"/account/bookings"}>
+        <Link
+          className="user-container flex items-center gap-2 text-sm" 
+          to={"/account/bookings"}
+        >
           My Bookings
         </Link>
-        <Link className="user-container flex items-center gap-2 " to={"/account/places"}>
+        <Link
+          className="user-container flex items-center gap-2 text-sm" 
+          to={"/account/places"}
+        >
           My trip offers
         </Link>
-        <Link className="user-container flex items-center gap-2" to={"/account/Myrequests"}>
+        <Link
+          className="user-container flex items-center gap-2 text-sm" 
+          to={"/account/Myrequests"}
+        >
           My trip Requests
         </Link>
       </ul>
@@ -31,4 +39,3 @@ const DropDownProfile = () => {
 };
 
 export default DropDownProfile;
-
