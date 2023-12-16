3f96095e44af7f8f180cb6b902fca2e743ba2b54
hikenrides <cephas@hikenrides.com>
2023-12-16 11:01:04 +0200

    polishing code

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,92 +1,23 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-  GoogleAuthProvider,
-  signInWithPopup,
-  RecaptchaVerifier,
-  signInWithPhoneNumber,
-} from "/opt/build/repo/client/node_modules/firebase/auth";
-import { auth } from "./firebase";
+import {createContext, useEffect, useState} from "react";
 import axios from "axios";
 import {data} from "autoprefixer";
 
-
 export const UserContext = createContext({});
 
-
-export function UserAuthContextProvider({ children }) {
-  const [user, setUser] = useState({});
+export function UserContextProvider({children}) {
+  const [user,setUser] = useState(null);
   const [ready,setReady] = useState(false);
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      console.log("Auth", currentuser);
-      if (currentuser) {
-        setUser(currentuser);
-      }
-    });
-  
-    // Move axios call outside the if condition
-    axios.get('/profile').then(({ data }) => {
-      setUser(data);
-      setReady(true);
-    });
-  
-    return () => {
-      unsubscribe();
-    };
+    if (!user) {
+      axios.get('/profile').then(({data}) => {
+        setUser(data);
+        setReady(true);
+      });
+    }
   }, []);
-  
-
-  function logIn(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
-  }
-  function signUp(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
-  }
-  function logOut() {
-    return signOut(auth);
-  }
-  function googleSignIn() {
-    const googleAuthProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleAuthProvider);
-  }
-
-  function setUpRecaptha(number) {
-    const recaptchaVerifier = new RecaptchaVerifier(
-      "recaptcha-container",
-      {},
-      auth
-    );
-    recaptchaVerifier.render();
-    return signInWithPhoneNumber(auth, number, recaptchaVerifier);
-  }
-
-  
-  
-        
   return (
-    <UserContext.Provider
-      value={{
-        user,
-        logIn,
-        signUp,
-        logOut,
-        googleSignIn,
-        setUpRecaptha,
-        setUser,
-        ready
-      }}
-    >
+    <UserContext.Provider value={{user,setUser,ready}}>
       {children}
     </UserContext.Provider>
   );
-  
-}
-
-export function useUserAuth() {
-  return useContext(UserContext);
 }
