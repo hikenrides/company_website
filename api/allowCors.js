78dde4da38fa3c8ec24c8db2418bfd22de8277f2
hikenrides <cephas@hikenrides.com>
2023-12-18 23:14:25 +0200

    ......

--- api/allowCors.js
+++ api/allowCors.js
@@ -1,19 +1,15 @@
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', 'https://hikenrides.com')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'https://hikenrides.com')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'https://hikenrides.com'
-  )
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS') // Add other methods as needed
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type') // Add other headers as needed
   if (req.method === 'OPTIONS') {
     res.status(200).end()
     return
   }
   return await fn(req, res)
 }
+
 
 const handler = (req, res) => {
   const d = new Date()
