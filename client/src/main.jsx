e94eb0de1f311c8a7dca85bd4a5e0f3ca78a025c
hikenrides <cephas@hikenrides.com>
2023-12-18 16:09:58 +0200

    .....

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from '/opt/build/repo/client/node_modules/react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
