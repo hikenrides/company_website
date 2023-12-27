591616bff3aa839c6bdd7edd871a2cfc8b0d42b6
hikenrides <cephas@hikenrides.com>
2023-12-27 07:59:54 +0200

    ...........

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,68 +1,47 @@
-import { Link, Navigate } from "react-router-dom";
-import { useContext, useState } from "react";
+import {Link, Navigate} from "react-router-dom";
+import {useContext, useState} from "react";
 import axios from "axios";
-import { UserContext } from "../UserContext.jsx";
+import {UserContext} from "../UserContext.jsx";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-  const { setUser } = useContext(UserContext);
-
+  const {setUser} = useContext(UserContext);
   async function handleLoginSubmit(ev) {
     ev.preventDefault();
     try {
-      const { data } = await axios.post("/login", { email, password });
+      const {data} = await axios.post('/login', {email,password});
       setUser(data);
-      alert("Login successful");
+      alert('Login successful');
       setRedirect(true);
     } catch (e) {
-      alert("Login failed");
+      alert('Login failed');
     }
   }
 
   if (redirect) {
-    return <Navigate to={"/account/trips"} />;
+    return <Navigate to={'/'} />
   }
 
   return (
-    <div className="mt-4 grow flex flex-col items-center justify-around">
+    <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-3xl text-center mb-4">
-          Login
-        </h1>
+        <h1 className="text-4xl text-center mb-4">Login</h1>
         <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
-          <input
-            type="email"
-            placeholder="your@email.com"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <div className="password-input">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="password"
-              value={password}
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
-            />
-            <label>Show Password</label>
-          </div>
+          <input type="email"
+                 placeholder="your@email.com"
+                 value={email}
+                 onChange={ev => setEmail(ev.target.value)} />
+          <input type="password"
+                 placeholder="password"
+                 value={password}
+                 onChange={ev => setPassword(ev.target.value)} />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
-            Don't have an account yet?{" "}
-            <Link className="underline text-black" to={"/register"}>
-              Register now
-            </Link>
+            Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
           </div>
         </form>
-        {/* Google Sign-In Button */}
-        
       </div>
     </div>
   );
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,14 +1,15 @@
-import {useContext, useState} from "react";
-import {UserContext} from "../UserContext.jsx";
-import {Link, Navigate, useParams} from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../UserContext.jsx";
+import { Link, Navigate, useParams } from "react-router-dom";
 import axios from "axios";
 import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
+import RequestsPage from "./RequestsPage.jsx";
 
 export default function ProfilePage() {
-  const [redirect,setRedirect] = useState(null);
-  const {ready,user,setUser} = useContext(UserContext);
-  let {subpage} = useParams();
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+  let { subpage } = useParams();
   if (subpage === undefined) {
     subpage = 'profile';
   }
@@ -19,28 +20,46 @@ export default function ProfilePage() {
     setUser(null);
   }
 
+  function deposit() {
+    // Add your logic for deposit functionality here
+  }
+
+  function withdraw() {
+    // Add your logic for deposit functionality here
+  }
+
   if (!ready) {
     return 'Loading...';
   }
 
   if (ready && !user && !redirect) {
-    return <Navigate to={'/login'} />
+    return <Navigate to={'/login'} />;
   }
 
   if (redirect) {
-    return <Navigate to={redirect} />
+    return <Navigate to={redirect} />;
   }
   return (
-    <div>
-      <AccountNav />
+    <div className="mb-64">
+      <div className="hidden md:block">
+        <AccountNav />
+      </div>
       {subpage === 'profile' && (
-        <div className="text-center max-w-lg mx-auto">
+        <div className="text-center max-w-lg mx-auto text-white">
           Logged in as {user.name} ({user.email})<br />
-          <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+          <h2 className="text-xl" style={{ color: 'black', marginRight: '8px' }}>Balance: R0</h2>
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <button onClick={deposit} className="bg-gray-400 text-white inline-flex gap-1 py-2 px-6 rounded-full max-w-sm mt-2">Deposit</button>
+            <button onClick={withdraw} className="bg-gray-400 text-white inline-flex gap-1 py-2 px-6 rounded-full max-w-sm mt-2">Withdraw</button>
+            <button onClick={logout} className="primary max-w-sm mt-10">Logout</button>
+          </div>
         </div>
       )}
       {subpage === 'places' && (
         <PlacesPage />
+      )}
+      {subpage === 'requests' && (
+        <RequestsPage />
       )}
     </div>
   );
