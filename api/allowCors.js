90d90e599b917e60a00f05bbb3e004c8defdcd30
hikenrides <cephas@hikenrides.com>
2024-01-03 00:40:21 +0200

    login and sign up error debugging

--- api/allowCors.js
+++ api/allowCors.js
@@ -1,14 +1,17 @@
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', 'https://hikenrides.com')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'https://hikenrides.com'
+  )
   if (req.method === 'OPTIONS') {
     res.status(200).end()
     return
   }
-
   return await fn(req, res)
 }
 
