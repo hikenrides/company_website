e815b67a4c2bc871bbfbc6af32a995ec853009d6
hikenrides <cephas@hikenrides.com>
2024-06-25 18:49:08 +0200

    ....

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,22 @@
-import { Link, Navigate } from "react-router-dom";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
+import { Link as RouterLink, Navigate } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../UserContext.jsx";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Link from '@mui/material/Link';  // Rename Link to avoid conflict
+
+const defaultTheme = createTheme();
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -27,43 +42,82 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="mt-4 grow flex flex-col items-center justify-around">
-      <div className="mb-64">
-        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-3xl text-center mb-4">
-          Login
-        </h1>
-        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
-          <input
-            type="email"
-            placeholder="your@email.com"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <div className="password-input">
-            <input
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginBottom: 10,  // Add marginBottom to push the footer down
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleLoginSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
               type={showPassword ? "text" : "password"}
-              placeholder="password"
+              id="password"
+              autoComplete="current-password"
               value={password}
               onChange={(ev) => setPassword(ev.target.value)}
             />
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
+            <FormControlLabel
+              control={
+                <Checkbox 
+                  checked={showPassword}
+                  onChange={() => setShowPassword(!showPassword)} 
+                  color="primary" 
+                />
+              }
+              label="Show Password"
             />
-            <label>Show Password</label>
-          </div>
-          <button className="primary">Login</button>
-          <div className="text-center py-2 text-gray-500">
-            Don't have an account yet?{" "}
-            <Link className="underline text-black" to={"/register"}>
-              Register now
-            </Link>
-          </div>
-        </form>
-       
-        
-      </div>
-    </div>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container>
+               {/*<Grid item xs>
+                <Link component={RouterLink} to="/forgot-password" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>*/}
+              <Grid item xs>
+                <Link component={RouterLink} to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,21 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
 import axios from "axios";
-import './TermsAndConditions.css';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Link from '@mui/material/Link';  // Rename Link to avoid conflict
+
+const defaultTheme = createTheme();
 
 export default function RegisterPage() {
   const [name, setName] = useState("");
@@ -64,106 +78,199 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="mt-4 grow flex items-center justify-around">
-      <div className="mb-64">
-        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-3xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto" onSubmit={registerUser}>
-          <input
-            type="text"
-            placeholder="Names and Surname"
-            value={name}
-            onChange={(ev) => setName(ev.target.value)}
-          />
-          <select
-            value={gender}
-            onChange={(ev) => setGender(ev.target.value)}
-          >
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-          <input
-            type="text"
-            placeholder="Phone number"
-            value={phone_number}
-            onChange={(ev) => setNumber(ev.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Age"
-            value={age}
-            onChange={(ev) => setAge(ev.target.value.slice(0, 3))}
-          />
-          <input
-            type="email"
-            placeholder="your@email.com"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <select
-            value={isDriver}
-            onChange={(ev) => setIsDriver(ev.target.value)}
-          >
-            <option value="">Are you a driver?</option>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-          {isDriver === "Yes" && (
-            <>
-              <input
-                type="text"
-                placeholder="Driver's License No"
-                value={driverLicense}
-                onChange={(ev) => setDriverLicense(ev.target.value)}
-              />
-            </>
-          )}
-
-<div className="password-field relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              value={password}
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-            <button className="eye-icon absolute right-0 top-0 m-2" onClick={togglePasswordVisibility}>
-              {showPassword ? (
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="20" viewBox="0 0 640 512">
-                <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/>
-              </svg>
-              ) : (
-                
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512">
-                <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
-              </svg>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box component="form" noValidate onSubmit={registerUser} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Names and Surname"
+                  value={name}
+                  onChange={(ev) => setName(ev.target.value)}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  select
+                  required
+                  fullWidth
+                  id="gender"
+                  label="Gender"
+                  name="gender"
+                  value={gender}
+                  onChange={(ev) => setGender(ev.target.value)}
+                  SelectProps={{
+                    native: true,
+                  }}
+                >
+                  <option value=""></option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="phone_number"
+                  label="Phone number"
+                  name="phone_number"
+                  value={phone_number}
+                  onChange={(ev) => setNumber(ev.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="age"
+                  label="Age"
+                  name="age"
+                  value={age}
+                  onChange={(ev) => setAge(ev.target.value.slice(0, 3))}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(ev) => setEmail(ev.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  select
+                  required
+                  fullWidth
+                  id="isDriver"
+                  label="Are you a driver?"
+                  name="isDriver"
+                  value={isDriver}
+                  onChange={(ev) => setIsDriver(ev.target.value)}
+                  SelectProps={{
+                    native: true,
+                  }}
+                >
+                  <option value=""></option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </TextField>
+              </Grid>
+              {isDriver === "Yes" && (
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="driverLicense"
+                    label="Driver's License No"
+                    name="driverLicense"
+                    value={driverLicense}
+                    onChange={(ev) => setDriverLicense(ev.target.value)}
+                  />
+                </Grid>
               )}
-            </button>
-          </div>
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(ev) => setConfirmPassword(ev.target.value)}
-          />
-
-          <div className="terms-checkbox">
-            <input
-              type="checkbox"
-              checked={agreeTerms}
-              onChange={() => setAgreeTerms(!agreeTerms)}
-            />
-            <label className="back-link2">
-              I agree to the <Link to="/Terms and Conditions" target="_blank">Terms and Conditions</Link>
-            </label>
-          </div>
-
-          <button className="primary">Register</button>
-          <div className="text-center py-2 text-gray-500">
-            Already a member? <Link className="underline text-black" to={"/login"}>Login</Link>
-          </div>
-        </form>
-      </div>
-    </div>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={(ev) => setPassword(ev.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type={showPassword ? "text" : "password"}
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(ev) => setConfirmPassword(ev.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox 
+                      checked={showPassword}
+                      onChange={togglePasswordVisibility} 
+                      color="primary" 
+                    />
+                  }
+                  label="Show Password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox 
+                      checked={agreeTerms}
+                      onChange={() => setAgreeTerms(!agreeTerms)} 
+                      color="primary" 
+                    />
+                  }
+                  label={
+                    <>
+                      I agree to the{' '}
+                      <RouterLink to="/Terms and Conditions" target="_blank">
+                        Terms and Conditions
+                      </RouterLink>
+                    </>
+                  }
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Register
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link component={RouterLink} to="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
