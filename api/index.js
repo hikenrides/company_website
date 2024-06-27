31250519cfd9029477ad9a7268f8e93b7e2e9f83
hikenrides <cephas@hikenrides.com>
2024-06-28 00:37:27 +0200

    ...

--- api/index.js
+++ api/index.js
@@ -178,7 +178,7 @@ app.post('/login', async (req, res) => {
           res.cookie("token", token, {
             httpOnly: true,
             secure: true, // Ensure secure flag is set
-            sameSite: "strict", // Adjust as necessary
+            sameSite: "None", // Adjust as necessary
             maxAge: 24 * 60 * 60 * 1000, // Set token expiration time
           }).json(userDoc);
         }
