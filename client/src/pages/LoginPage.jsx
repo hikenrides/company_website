3e8ae3692bedcabd7d2b1468df8329e69fc2bf56
hikenrides <cephas@hikenrides.com>
2024-07-30 01:47:00 +0200

    ...

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -147,13 +147,16 @@ export default function LoginPage() {
             >
               Login
             </Button>
-            <GoogleOAuthProvider clientId="300890038465-pim80rkka1tn10ro5h80g4ncctmqeg4u.apps.googleusercontent.com">
-              <GoogleLogin
-                onSuccess={handleGoogleSuccess}
-                onFailure={handleGoogleFailure}
-                cookiePolicy={'single_host_origin'}
-              />
-            </GoogleOAuthProvider>
+            <div style={{ marginBottom: '1rem', textAlign: 'center' }}>
+              <GoogleOAuthProvider clientId="300890038465-pim80rkka1tn10ro5h80g4ncctmqeg4u.apps.googleusercontent.com">
+                <GoogleLogin
+                  onSuccess={handleGoogleSuccess}
+                  onFailure={handleGoogleFailure}
+                  cookiePolicy={'single_host_origin'}
+                  width="100%"
+                />
+              </GoogleOAuthProvider>
+            </div>
             <Grid container>
               <Grid item xs>
                 <Link component={RouterLink} to="/register" variant="body2">
