03192ed74613fb312f134ed6fe8b7cf98782315e
hikenrides <146217396+hikenrides@users.noreply.github.com>
2024-11-10 12:37:30 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -198,10 +198,7 @@ app.get('/auth/google/callback', async (req, res) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.NODE_ENV === 'production'
-  ? "https://hikenrides.com/auth/google/callback"
-  : "http://localhost:5173/auth/google/callback"
-
+  callbackURL: "http://localhost:3000/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
